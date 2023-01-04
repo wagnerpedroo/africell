4d13e1d7d65e1154259a10717dedd0020fbeda04
Wagner Pedro <wagnerpedro355@gmail.com>
2023-01-04 18:03:39 -0300

    retirei o jquery, vou fazer tudo com js puro

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 let currentSlide = 1;
-let $slider = $(".slider");
-let slideCount = $slider.children();
+let $slider = $(".slides");
+let slideCount = $slider.children().length;
+let timerInterval = 2 * 1000;
+//console.log(slideCount);
 
+setInterval(()=>{
+    $slider.animate({
+        marginLeft: '-=100%',
+    }, 800);
+}, timerInterval);
